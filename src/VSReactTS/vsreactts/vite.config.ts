9e2ebaeff06981863e8e6f02a8a42a790f5e2c8e
jerviscui <cuijervis@126.com>
2026-02-12 15:39:16 +0800

    Add VS template project

---
+++ src/VSReactTS/vsreactts/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [plugin()],
+    server: {
+        host: '127.0.0.1',
+        port: 43433,
+    }
+})
